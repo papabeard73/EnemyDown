5e8e93b8c067e3137167b4b09adb8d97f99bbf04
yasuda <eriko@csplan.jp>
2025-06-01 15:41:53 +0900

    DAY15 敵を倒すと点数を獲得して合計を加算する

--- src/main/java/plugin/enemyDown/Main.java
+++ src/main/java/plugin/enemyDown/Main.java
@@ -1,19 +1,21 @@
 package plugin.enemyDown;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.enemyDown.command.EnemyDownCommand;
 import plugin.enemyDown.command.GetBedCommand;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin {
   @Override
   public void onEnable() {
     // Plugin startup logic
-    // イベントリスナーを登録
-    Bukkit.getPluginManager().registerEvents(this, this);
 
-    getCommand("enemyDown").setExecutor(new EnemyDownCommand());
+    EnemyDownCommand enemyDownCommand = new EnemyDownCommand();
+
+    // イベントリスナーを登録
+    Bukkit.getPluginManager().registerEvents(enemyDownCommand,this);
+
+    getCommand("enemyDown").setExecutor(enemyDownCommand);
     getCommand("getBed").setExecutor(new GetBedCommand());
   }
 }
--- src/main/java/plugin/enemyDown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemyDown/command/EnemyDownCommand.java
@@ -1,24 +1,42 @@
 package plugin.enemyDown.command;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.SplittableRandom;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.NotNull;
 
-public class EnemyDownCommand implements CommandExecutor {
+public class EnemyDownCommand implements CommandExecutor, Listener {
+  private Player player;
+  private int score;
 
   @Override
   public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
       @NotNull String label, @NotNull String[] args) {
       if (sender instanceof Player player){
+        this.player = player;
+
         player.setHealth(20);
         player.setFoodLevel(20);
+
+        PlayerInventory inventory = player.getInventory();
+        inventory.setHelmet(new ItemStack(Material.DIAMOND_HELMET));
+        inventory.setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
+        inventory.setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+        inventory.setBoots(new ItemStack(Material.DIAMOND_BOOTS));
+        inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_SWORD));
 
         // ワールドの情報を変数に持つ。
         World world = player.getWorld();
@@ -26,10 +44,10 @@ public class EnemyDownCommand implements CommandExecutor {
         Location enemySpawnLocation = enemySpawnLocation(player, world);
 
         int randomIntEnemy = new SplittableRandom().nextInt(2);
-        List<EntityType> enemyList = List.of(EntityType.ZOMBIE, EntityType.CREEPER);
+        List<EntityType> enemyList = List.of(EntityType.ZOMBIE, EntityType.SKELETON);
 
         world.spawnEntity(enemySpawnLocation, enemyList.get(randomIntEnemy));
-      };
+      }
     return false;
   }
 
@@ -52,5 +70,20 @@ public class EnemyDownCommand implements CommandExecutor {
     double y = playerLocation.getY();
     double z = playerLocation.getZ() + randomIntZ;
     return new Location(world, x,y,z);
+  }
+
+  @EventHandler
+  public void onEnemyDeath (EntityDeathEvent e) {
+      Player player = e.getEntity().getKiller();
+      if (Objects.isNull(player)){
+        return;
+      }
+      if (Objects.isNull(this.player)){
+        return;
+      }
+      if(this.player.getName().equals(player.getName())) {
+        score += 10;
+        player.sendMessage("敵を倒した！現在のスコアは" + score);
+    }
   }
 }
